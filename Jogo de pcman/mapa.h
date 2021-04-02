579fe726df4dcd350b385b23b470c5bf1aa25881
ss <setrotec@outlook.com>
2021-04-02 17:46:43 -0300

    criando arquivo mapas.h

---
+++ Jogo de pcman/mapa.h
@@ -0,0 +1,24 @@
+
+/*função struct: permite agrupar uma "coleção" de variáveis do "tipo" Mapa.
+    essa struct quando chamada terá uma matrix, mais duas variaveis do tipo int.
+ */
+struct mapa{
+
+    /*Declarando uma matrix de char. Os dois ** após o tipo da variável 
+      decalra um ponteiro de ponteiro para char.
+     */
+    char** matriz;
+    int linhas;
+    int colunas;
+};
+
+typedef struct mapa MAPA;
+
+
+/*Declarando as funções  do aquivo jogopcman.c em ajogopcman.h para que o compilador
+    possa compilar o codigo sem se preocupar cma ordem das funções dentro do programa.
+ */
+void liberamapa(MAPA* m);
+void alocamapa(MAPA* m);
+void lemapa(MAPA* m);
+void imprimemapa(MAPA* m);
